605f725d7ec4123e790b299961db1004b74733f0
Arthur Henrique <arthurhenrique.dev@gmail.com>
2025-10-27 00:03:57 -0300

    feat: pug rendering

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import 'dotenv/config';
 
 import { connectDB } from './config/db.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.json());
+app
+  .set('view engine', 'pug')
+  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(__dirname, '..', 'public')))
+  .use(express.json());
 
 await connectDB();
 app.listen(3000);
