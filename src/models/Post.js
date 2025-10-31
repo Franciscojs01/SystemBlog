f8d664181bb847c1c19710bfff40fe2192f221a0
Arthur Henrique <arthurhenrique.dev@gmail.com>
2025-10-30 21:05:13 -0300

    chore: added timestamps

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 
 /**
+ * author: ObjectId;
  * title: String;
  * content: String;
- * author: ObjectId;
- * createdAt: Date;
- * updatedAt: Date;
+ * thumbnail: String (URL);
  */
 
 const definition = {
   author: { type: String, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
+  thumbnail: { type: String, required: true },
 };
 
-const postSchema = new mongoose.Schema(definition);
+const postSchema = new mongoose.Schema(definition, { timestamps: true });
 
 export const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
