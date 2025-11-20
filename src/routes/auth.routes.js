4b0a6ecdccfe14260617141a7a7810c6250d70e4
Arthur Henrique <arthurhenrique.dev@gmail.com>
2025-11-19 22:36:41 -0300

    refactor: some code cleaning

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,12 +7,11 @@ const router = Router();
 
 const JWT_SECRET = 'sua-chave-super-secreta-e-longa-12345';
 
-router.post('/login', async (req, res) => {
-  // Use 'email' e 'password' conforme definidos no req.body
-  const { email, password } = req.body;
-
+router.post('/login', async ({ email, password }, res) => {
   if (!email || !password) {
-    return res.status(400).json({ mensagem: 'Email e senha são obrigatórios.' });
+    return res
+      .status(400)
+      .json({ mensagem: 'Email e senha são obrigatórios.' });
   }
 
   try {
@@ -39,9 +38,8 @@ router.post('/login', async (req, res) => {
 
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
     return res.status(200).json({ token: token });
-
   } catch (error) {
-    console.error("Erro interno do servidor ao tentar autenticar: ", error);
+    console.error('Erro interno do servidor ao tentar autenticar: ', error);
 
     return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
   }
