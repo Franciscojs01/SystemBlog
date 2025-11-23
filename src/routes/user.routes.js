99fe39bc75932c7e046137c2cff25c34022e1bd1
Arthur Henrique <arthurhenrique.dev@gmail.com>
2025-11-22 21:28:38 -0300

    chore: data validation in users routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,20 +3,54 @@ import { Router } from 'express';
 import UserController from '../controllers/user.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import adminMiddleware from '../middlewares/admin.middleware.js';
+import {
+  userFullRules,
+  userIdRules,
+  userPatchRules,
+} from '../validators/user.validators.js';
 
 const router = Router();
 
-router.post('/', UserController.create);
+// POST /users → Cadastrar usuário (qualquer cargo)
+router.post('/', userFullRules, UserController.create);
 
-router.get('/', UserController.findAll);
-// router.get('/', adminMiddleware, authMiddleware, UserController.findAll);
+// GET /users → Listar todos os usuários (apenas manager)
+router.get('/', adminMiddleware, authMiddleware, UserController.findAll);
 
-router.get('/:id', adminMiddleware, authMiddleware, UserController.findById);
+// GET /users/:id → Listar usuário pelo ID (apenas manager)
+router.get(
+  '/:id',
+  userIdRules,
+  adminMiddleware,
+  authMiddleware,
+  UserController.findById,
+);
 
-router.put('/:id', adminMiddleware, authMiddleware, UserController.update);
+// PUT /users/:id → Editar completamente o usuário (apenas manager)
+router.put(
+  '/:id',
+  userFullRules,
+  adminMiddleware,
+  authMiddleware,
+  UserController.update,
+);
 
-router.patch('/:id', adminMiddleware, authMiddleware, UserController.update);
+// PATCH /users/:id → Editar parcialmente o usuário (qualquer cargo)
+router.patch(
+  '/:id',
+  userPatchRules,
+  adminMiddleware,
+  authMiddleware,
+  UserController.update,
+);
 
-router.delete('/:id', adminMiddleware, authMiddleware, UserController.delete);
+// DELETE /users/:id → Apagar o usuário (apenas manager)
+router.delete(
+  '/:id',
+  userIdRules,
+  adminMiddleware,
+  authMiddleware,
+  UserController.delete,
+);
 
 export default router;
