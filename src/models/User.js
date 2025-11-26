d524032e7d38ed85090097eb3876c05f95f61b3d
Arthur Henrique <arthurhenrique.dev@gmail.com>
2025-11-26 01:46:15 -0300

    refactor: removing unecessay comments

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose';
 
-/**
- * name: String;
- * email: String;
- * password: String (hash);
- * role: ('manager' | 'author' | 'viewer')[];
- */
-
 const definition = {
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
